6dac5f690226bc3d892354c249f6b74b91c7df1a
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-08-12 03:04:43 -0700

    Completes lab: creates a one-to-many object relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None) -> None:
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type in self.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception("Pet type not allowed")
+
 
 class Owner:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception(f"{pet} is not a valid pet")
+        pet.owner = self
+
+    def get_name(pet):
+        return pet.name
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda x: x.name)
